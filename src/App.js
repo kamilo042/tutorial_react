3a6c6461e702dd0cbd156a05e11ce37238eab584
kamilo042 <kamilo042.cp@gmail.com>
2022-07-27 16:35:28 -0500

    proyecto terminado con algunas cosas por mejorar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Background from './componentes/Background';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
         <div className='contenedor-principal'>
           <h1>Algunas clases interesantes de D&D</h1>  
-          <Background/>
-        </div>
+          <Background
+            nombre= 'Paladin'
+            origen= 'la justicia'
+            imagen='paladin'
+            cargo='Guardian'
+            empresa='el Cielo'
+            background='The paladin is one of the standard playable character classes in most editions of the Dungeons & Dragons fantasy role-playing game. The paladin is a holy knight, crusading in the name of good and order, and is a divine spellcaster. '/>
+          
+          <Background 
+            nombre='thief'
+            origen='ciudad daga'
+            imagen='thief'
+            cargo='coleccionista de tesoros'
+            empresa='cloacas de la ciudad'
+            background='The rogue or thief is one of the standard playable character classes in most editions of the Dungeons & Dragons fantasy role-playing game. A rogue is a versatile character, capable of sneaky combat and nimble tricks'
+          />
+          <Background 
+            nombre='Druida'
+            origen='los bosques profundos'
+            imagen='druid'
+            cargo='Amo de la naturaleza'
+            empresa='domador de animales'
+            background='The druid is a playable character class in the Dungeons & Dragons fantasy role-playing game.[1][2] Druids wield nature-themed magic. Unlike clerics, druids do not have special powers against undead and, in some editions, cannot use metal armor. Druids have a unique ability that allows them to change into various animal forms, and various other qualities that assist them in natural settings. '
+
+          />
+               </div>
     </div>
   );
 }
--- src/componentes/Background.jsx
+++ src/componentes/Background.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
-    function Background() {
+import '../hojas-de-estilo/Background.css'
+    function Background(props) {
       return (
         <div className='contenedor-background'>
             <img
               className='imagen-background'
-              src={require ('../imagenes/paladin.jpg')}
+              src={require (`../imagenes/background-${props.imagen}.jpg`)}
               alt='imagen de paladin'/>
             <div className='contenedor-texto-background'>
-                <p className='nombre-background'>Paladin de la justicia </p>
-                <p className='cargo-background'> Guardian de los cielos</p>
-                <p className='texto-background'>The paladin is one of the standard playable character classes in most editions of the Dungeons & Dragons fantasy role-playing game. The paladin is a holy knight, crusading in the name of good and order, and is a divine spellcaster. </p>
+                <p className='nombre-background'><strong>{props.nombre}</strong> de {props.origen}</p>
+                <p className='cargo-background'> {props.cargo} de {props.empresa}</p>
+                <p className='texto-background'>"{props.background}"</p>
             </div>
         </div>
       );
